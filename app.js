2298870182ef6aaadb30c3fd0b1153142afe23a1
zawixmc <ulmanekkacperek@gmail.com>
2025-09-18 14:51:20 +0200

    PERFORMANCE

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-﻿import { db, collection, addDoc, getDocs, query, orderBy, onSnapshot, doc, updateDoc, deleteDoc, where } from './firebase-config.js';
+﻿function setViewportHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setViewportHeight();
+window.addEventListener('resize', setViewportHeight);
+window.addEventListener('orientationchange', () => {
+    setTimeout(setViewportHeight, 100);
+});
+import { db, collection, addDoc, getDocs, query, orderBy, onSnapshot, doc, updateDoc, deleteDoc, where } from './firebase-config.js';
 
 const { useState, useEffect, useRef } = React;
 
